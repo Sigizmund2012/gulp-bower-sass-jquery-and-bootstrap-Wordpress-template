b71a82440003c51309944950e9553788be00f50b
Smartfreelancer <segeda823@mail.ru>
2016-02-28 22:25:19 +0300

    Add services section

--- src/index.php
+++ src/index.php
@@ -1,4 +1,63 @@
 <?php get_header(); ?>
-<?php get_sidebar( 'mainslider' ); ?>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<?php
+						$category_services = get_category(194);
+						$url = home_url();
+						?>
+						<div class="services-cat">
+							<h1>Услуги, которые вы можете заказать у меня</h1>
+							<p class="service-cat__description"><?php echo $category_services->description ?>
+								<a href="<?php echo $url . "/category/" . $category_services->slug . "/" ?>" class="services-cat__link">Полный список
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<?php
+					$args = array('posts_per_page' => 4, 'category' => 194);
+					$myposts = get_posts($args);
+					$i = 0;
+					?>
+
+					<?php foreach ($myposts as $post) : setup_postdata($post); ?>
+
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<section class="post-service">
+								<div class="post-service__icon post-service__icon_number_<?php echo ++$i ?>"></div>
+								<h2 class="post-service__title">
+									<?php echo $post->post_title ?>
+								</h2>
+								<div class="post-service__content">
+									<?php
+									$postcontent = $post->post_content;
+									$string_width = 200; // limit of string characters
+									if (strlen( $postcontent ) > $string_width)
+									{
+										 $postcontent = wordwrap( $postcontent, $string_width );
+										 $postcontent = substr( $postcontent, 0, strpos( $postcontent, "\n"));
+									}
+									echo $postcontent;
+									?>
+								</div>
+								<div class="post-service__link">
+								<a href="<?php the_permalink(); ?>" class="post-service__link_anchor">Подробнее</a>
+								</div>
+							</section>
+						</div>
+
+						<?php
+					endforeach;
+					wp_reset_postdata();
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php get_footer(); ?>
