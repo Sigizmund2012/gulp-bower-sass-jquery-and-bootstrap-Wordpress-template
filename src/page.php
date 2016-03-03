459764382cb6c3f8c10671de3a0e383ea9e1e7dc
Smartfreelancer <segeda823@mail.ru>
2016-03-03 16:05:51 +0300

    End page.php

--- src/page.php
+++ src/page.php
@@ -1,27 +1,45 @@
 <?php get_header(); ?>
 
 <?php
-	if( is_front_page() ){
-		get_sidebar( 'services' );
-		get_sidebar( 'whyme' );
-	}
-	else{
-?>
+if (is_front_page()) {
+	get_sidebar('services');
+	get_sidebar('whyme');
+} else {
+	?>
 
-	<div class="container-fluid">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<article role="main">
+		<div class="container-fluid content-top">
+		<div class="row">
+		<div class="container">
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class="content-top__heading"><?php the_title(); ?></h1>
+			</div>
+		</div>
+
+	</div>
+	</div>
+	</div>
+
+	<div class="container-fluid page-content-general">
 		<div class="row">
 			<div class="container">
-				<article role="main">
-					<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
-						<h1><?php the_title(); ?></h1>
-						<?php the_content(); ?>
-					<?php endwhile; ?>
-					<?php endif; ?>
-				</article>
+				<div class="row">
+					<div class="col-xs-12">
+							<div class="page-text">
+								<?php the_content(); ?>
+							</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+		</article>
+	<?php endwhile; ?>
+	<?php endif; ?>
 
-	<?php } ?>
+<?php } ?>
 
 <?php get_footer(); ?>
